3e8db020a7e939f4610e41bc025d2e8bb0323b38
farukest <abdullahfarukozden@gmail.com>
2023-01-21 19:44:29 +0300

    changes

--- runall.sh
+++ runall.sh
@@ -15,7 +15,7 @@ do
 		then 
 			echo "ALREADY RUNNING";
 		else
-			echo "STARTING BECAUSE IT HAS STOPPED - "pgrep lora_pkt_fwd$i;			
+			echo "STARTING BECAUSE IT HAS STOPPED - "pgrep lora_pkt_fwd$i			
 			echo "$i"
 			cd /home/ft/hs_ft_pf_$i/ && make -f Makefile
 			file=/home/ft/hs_ft_pf_$i/packet_forwarder/lora_pkt_fwd$i
@@ -31,79 +31,29 @@ do
 		
 done
 
+count_local_pf=$(pgrep -c local_pf)
+echo $count_local_pf
+if [[ $count_local_pf -gt 1 ]]; 
+then
+  echo "Killing that pktwds.."
+  pgrep local_pf | xargs kill
+fi
 
-# lora_pkt_fwd1 ve lora_pkt_fwd2 çalışır
-i=0
-while [ $i -ne 1 ]
-do
-		i=$(($i+1))
-		
-		FILE=/home/ft/hs_ft_pf_$i/Makefile
-		if [ -e "$FILE" ]; then
-			echo "Makefile exist so may compiled c and obj.. check and remove them.."
-			
-			# Check pktfwd exist
-			PKTFWD=/home/ft/hs_ft_pf_$i/packet_forwarder/lora_pkt_fwd$i
-			if [ -e "$PKTFWD" ]; then
-				echo "PKTFWD REMOVED.."
-				rm -rf /home/ft/hs_ft_pf_$i/packet_forwarder/lora_pkt_fwd$i
-			fi 
 
-			# check obj .o exist
-			PKTFWDOBJ=/home/hs_ft_pf_$i/packet_forwarder/obj/lora_pkt_fwd$i.o
-			if [ -e "$PKTFWDOBJ" ]; then
-				echo "PKTFWD .o REMOVED.."
-				rm -rf /home/hs_ft_pf_$i/packet_forwarder/obj/lora_pkt_fwd$i.o
-			fi 
-						
-			echo "Making new PKTFWD files and the OBJ .o files.."
-			# Create new pktfwd and the obj .o					
-			cd /home/ft/hs_ft_pf_$i/ && make -f Makefile
-			echo "Making files success.."
-			
-			
-			echo "Maked files moving and keeping and transferring.."
-			# Move pktfwd to to tmp and then remove folders and again move pktfwd to folder
-			mv /home/ft/hs_ft_pf_$i/packet_forwarder/lora_pkt_fwd$i /tmp/
-			rm -rf /home/ft/hs_ft_pf_$i
-			mkdir -p /home/ft/hs_ft_pf_$i/packet_forwarder/
-			mv /tmp/lora_pkt_fwd$i /home/ft/hs_ft_pf_$i/packet_forwarder/  
-			echo "Transferring success.."
-		fi       
-		
-done
-
-# normal pf çalışma metodu. servera göndermez locale gönderir. normal pf
-j=3
-FILE=/home/ft/hs_ft_pf_$j/Makefile
-if [ -e "$FILE" ]; then
-	echo "Makefile exist so may compiled c and obj.. check and remove them.."
-	
-	# Check pktfwd exist
-	PKTFWD=/home/ft/hs_ft_pf_$j/packet_forwarder/lora_pkt_fwd$j
-	if [ -e "$PKTFWD" ]; then
-		echo "PKTFWD REMOVED.."
-		rm -rf /home/ft/hs_ft_pf_$j/packet_forwarder/lora_pkt_fwd$j
-	fi 
-
-	# check obj .o exist
-	PKTFWDOBJ=/home/hs_ft_pf_$j/packet_forwarder/obj/lora_pkt_fwd$j.o
-	if [ -e "$PKTFWDOBJ" ]; then
-		echo "PKTFWD .o REMOVED.."
-		rm -rf /home/hs_ft_pf_$j/packet_forwarder/obj/lora_pkt_fwd$j.o
-	fi 
-				
-	echo "Making new PKTFWD files and the OBJ .o files.."
-	# Create new pktfwd and the obj .o					
-	cd /home/ft/hs_ft_pf_$j/ && make -f Makefile
-	echo "Making files success.."
-	
-	
-	echo "Maked files moving and keeping and transferring.."
-	# Move pktfwd to to tmp and then remove folders and again move pktfwd to folder
-	mv /home/ft/hs_ft_pf_$j/packet_forwarder/lora_pkt_fwd$j /tmp/
-	rm -rf /home/ft/hs_ft_pf_$j
-	mkdir -p /home/ft/hs_ft_pf_$j/packet_forwarder/
-	mv /tmp/lora_pkt_fwd$j /home/ft/hs_ft_pf_$j/packet_forwarder/  
-	echo "Transferring success.."
+if [[ $count_local_pf -eq 1 ]]; 
+then 
+	echo "ALREADY RUNNING";
+else
+	echo "STARTING BECAUSE IT HAS STOPPED - "pgrep local_pf			
+	cd /home/ft/hs_ft_pf_3/ && make -f Makefile
+	file_local_pf=/home/ft/hs_ft_pf_3/packet_forwarder/local_pf
+	[ -x $file_local_pf ] && R="Read = yes" || R="Read = No"
+	echo "$R is not executable"
+	if ! [ -x $file_local_pf ]; then
+		chmod 700 $file_local_pf
+		echo "$R is now executable..."
+	fi
+	cd /home/ft/hs_ft_pf_3/packet_forwarder/ && ./local_pf &
 fi       
+		
+
